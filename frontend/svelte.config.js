d0bfe76bdd28972b508a29616df07e6342a4d590
LigsQt <johnysaac01@gmail.com>
2026-08-18 01:18:28 +0800

    build: support both Docker and Vercel deployments

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,9 +1,15 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const isVercel = process.env.VERCEL;
+
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() },
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: isVercel ? adapterVercel() : adapterNode()
+  }
 };
 
 export default config;
